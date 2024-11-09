fb2c35121a9d257437d86d3577b7377690ac55f9
Aditi <adititakale01@gmail.com>
2024-11-09 23:27:40 +0100

    Added unit tests for math_quiz functions

--- math_quiz homework 2/math_quiz/tests_math_quiz.py
+++ math_quiz homework 2/math_quiz/tests_math_quiz.py
@@ -1,30 +1,25 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import RandomNumberGenerator, RandomOperator, solveFunction
 
+class TestMathQuiz(unittest.TestCase):
+    
+    def testRandomNumberGenerator(self):
+        result = RandomNumberGenerator(1, 10)
+        self.assertTrue(1 <= result <= 10, "Number is outside the specified range")
+    
+    def testRandomOperator(self):
+        operator = RandomOperator()
+        self.assertIn(operator, ['+', '-', '*'], "Operator is not valid")
+    
+    def testSolveFunction(self):
+        question, answer = solveFunction(5, 3, '+')
+        self.assertEqual(answer, 8, "Addition answer is incorrect")
+        
+        question, answer = solveFunction(5, 3, '-')
+        self.assertEqual(answer, 2, "Subtraction answer is incorrect")
+        
+        question, answer = solveFunction(5, 3, '*')
+        self.assertEqual(answer, 15, "Multiplication answer is incorrect")
 
-class TestMathGame(unittest.TestCase):
-
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
-
-    def test_function_B(self):
-        # TODO
-        pass
-
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
-
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
